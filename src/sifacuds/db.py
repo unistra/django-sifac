70a7ecd0589007e2935b127a11d6e784096e0f5f
Arnaud Grausem <arnaud.grausem@unistra.fr>
2012-07-11 12:36:24 +0000

    refs #1735 @1 - externalize filters building from query on Sifac DB connection

--- src/sifacuds/db.py
+++ src/sifacuds/db.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import logging
-import re
 import saprfc
 
 from django.conf import settings
@@ -17,7 +16,7 @@ class SifacDB(object):
         """ Init conn attr """
         self.__conn = conn
 
-    def __connect(self):
+    def _connect(self):
         """ Connection to SIFAC """
         if self.__conn is None:
             try:
@@ -27,7 +26,7 @@ class SifacDB(object):
             except Exception as e:
                 logger.critical("Can't connect to Sifac DB: %s" % str(e))
 
-    def __close(self):
+    def _close(self):
         """ Close connection from SIFAC """
         try:
             self.__conn.close()
@@ -35,30 +34,41 @@ class SifacDB(object):
         except Exception as e:
             logger.error("Can't close Sifac connection: %s" % str(e))
 
+    def _build_filtered_query(self, column, filters):
+        """
+        """
+        query = []
+        if filters:
+            query = []
+            for index, filter_ in enumerate(filters):
+                query.append("{0} LIKE '{1}'".format(column, filter_))
+                if index < len(filters) - 1:
+                    query.append("OR")
+        return query
+
     def query(self, table, columns, *filters):
         """
         """
+        values = []
+
         try:
-            self.__connect()
+            self._connect()
 
             iface = self.__conn.discover("RFC_READ_TABLE")
             iface.query_table.setValue(table)
             iface.FIELDS.setValue(columns)
-            if filters:
-                query = []
-                for index, filter_ in enumerate(filters):
-                    query.append("{0} LIKE '{1}'".format(columns[0], filter_))
-                    if index < len(filters) - 1:
-                        query.append("OR")
+
+            query = self._build_filtered_query(columns[0], filters)
+            if query:
                 iface.OPTIONS.setValue(query)
 
             self.__conn.callrfc( iface )
             values = iface.DATA.value
 
         except Exception as e:
-            logger.error("Can't exexcute query on Sifac DB: %s" % str(e))
+            logger.error("Can't execute query on Sifac DB: %s" % str(e))
 
         finally:
-            self.__close()
+            self._close()
 
         return values
